80cd67410ab9499edda756bf65d9044b6ee3a26b
Mario Reder <mreder1289@gmail.com>
2021-02-15 08:19:59 +0100

    discord login

--- sm64js/src/login.rs
+++ sm64js/src/login.rs
@@ -6,41 +6,44 @@ use paperclip::actix::{api_v2_errors, api_v2_operation, web, Apiv2Schema, Mounta
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
+pub static GOOGLE_CLIENT_ID: &str =
+    "1000892686951-dkp1vpqohmbq64h7jiiop9v6ic4t1mul.apps.googleusercontent.com";
+pub static DISCORD_CLIENT_ID: &str = "807123464414429184";
+
 #[derive(Debug, Deserialize)]
 struct Login {
     code: String,
 }
 
 #[derive(Debug, Serialize)]
-struct GoogleOAuthRequest {
+struct OAuth2Request {
     client_id: String,
     client_secret: String,
     code: String,
     grant_type: String,
     redirect_uri: String,
+    scopes: Option<String>,
 }
 
 #[derive(Debug, Deserialize)]
-struct GoogleOAuthResponse {
+struct GoogleOAuth2Response {
     id_token: String,
     expires_in: i64,
 }
 
-pub fn service() -> impl dev::HttpServiceFactory + Mountable {
-    web::scope("/login")
-        .service(web::resource("").route(web::post().to(login)))
-        .service(web::resource("/google").route(web::post().to(login_with_google)))
-        .service(web::resource("/discord").route(web::post().to(login_with_discord)))
+#[derive(Debug, Deserialize)]
+struct DiscordOAuth2Response {
+    access_token: String,
+    token_type: String,
+    expires_in: i64,
 }
 
-#[api_v2_operation(tags(Auth))]
-async fn login() -> String {
-    // TODO persist session
-    todo!()
+#[derive(Apiv2Schema, Debug, Serialize)]
+struct AuthorizedUserMessage {
+    username: Option<String>,
+    code: u8,
+    message: Option<String>,
 }
-
-pub static GOOGLE_CLIENT_ID: &str =
-    "1000892686951-dkp1vpqohmbq64h7jiiop9v6ic4t1mul.apps.googleusercontent.com";
 
 #[derive(Debug, Deserialize)]
 pub struct IdToken {
@@ -60,17 +63,44 @@ pub struct IdToken {
     pub locale: Option<String>,
 }
 
+#[derive(Debug, Deserialize)]
+struct DiscordUser {
+    id: String,
+    username: String,
+    discriminator: String,
+    avatar: Option<String>,
+    mfa_enabled: Option<bool>,
+    locale: Option<String>,
+    flags: Option<u32>,
+    premium_type: Option<u8>,
+    public_flags: Option<u32>,
+}
+
+pub fn service() -> impl dev::HttpServiceFactory + Mountable {
+    web::scope("/login")
+        .service(web::resource("").route(web::post().to(login)))
+        .service(web::resource("/google").route(web::post().to(login_with_google)))
+        .service(web::resource("/discord").route(web::post().to(login_with_discord)))
+}
+
+#[api_v2_operation(tags(Auth))]
+async fn login() -> String {
+    // TODO persist session
+    todo!()
+}
+
 #[api_v2_operation(tags(Auth))]
 async fn login_with_google(
     json: web::Json<Login>,
     _session: Session,
 ) -> Result<web::Json<AuthorizedUserMessage>, LoginError> {
-    let req = GoogleOAuthRequest {
+    let req = OAuth2Request {
         client_id: GOOGLE_CLIENT_ID.to_string(),
         client_secret: std::env::var("GOOGLE_CLIENT_SECRET").unwrap(),
         code: json.code.clone(),
         grant_type: "authorization_code".to_string(),
-        redirect_uri: "http://localhost:3060".to_string(),
+        redirect_uri: std::env::var("REDIRECT_URI").unwrap(),
+        scopes: None,
     };
     let request: SendClientRequest = awc::Client::default()
         .post("https://oauth2.googleapis.com/token")
@@ -79,7 +109,7 @@ async fn login_with_google(
     if !response.status().is_success() {
         return Err(LoginError::TokenExpired);
     };
-    let response: GoogleOAuthResponse = response.json().await?;
+    let response: GoogleOAuth2Response = response.json().await?;
 
     let request: SendClientRequest = awc::Client::default()
         .get(&format!(
@@ -93,7 +123,7 @@ async fn login_with_google(
     };
     let _response: IdToken = response.json().await?;
 
-    // TODO store session
+    // TODO store session and account
     Ok(web::Json(AuthorizedUserMessage {
         username: None,
         code: 1,
@@ -101,17 +131,49 @@ async fn login_with_google(
     }))
 }
 
-#[derive(Apiv2Schema, Debug, Serialize)]
-struct AuthorizedUserMessage {
-    username: Option<String>,
-    code: u8,
-    message: Option<String>,
-}
-
 #[api_v2_operation(tags(Auth))]
-async fn login_with_discord() -> String {
-    // TODO
-    todo!()
+async fn login_with_discord(
+    json: web::Json<Login>,
+    _session: Session,
+) -> Result<web::Json<AuthorizedUserMessage>, LoginError> {
+    let req = OAuth2Request {
+        client_id: DISCORD_CLIENT_ID.to_string(),
+        client_secret: std::env::var("DISCORD_CLIENT_SECRET").unwrap(),
+        code: json.code.clone(),
+        grant_type: "authorization_code".to_string(),
+        redirect_uri: std::env::var("REDIRECT_URI").unwrap(),
+        scopes: Some("guilds".to_string()),
+    };
+    let request: SendClientRequest = awc::Client::default()
+        .post("https://discord.com/api/oauth2/token")
+        .send_form(&req);
+    let mut response = request.await?;
+    if !response.status().is_success() {
+        return Err(LoginError::TokenExpired);
+    };
+    let response: DiscordOAuth2Response = response.json().await?;
+    dbg!(&response);
+
+    let request: SendClientRequest = awc::Client::default()
+        .get("https://discord.com/api/users/@me")
+        .header(
+            awc::http::header::AUTHORIZATION,
+            format!("{} {}", response.token_type, response.access_token),
+        )
+        .send();
+    let mut response = request.await?;
+    if !response.status().is_success() {
+        return Err(LoginError::TokenExpired);
+    };
+    let response: DiscordUser = response.json().await?;
+    dbg!(&response);
+
+    // TODO store session and account
+    Ok(web::Json(AuthorizedUserMessage {
+        username: Some(format!("{}#{}", response.username, response.discriminator)),
+        code: 1,
+        message: None,
+    }))
 }
 
 #[api_v2_errors(code = 400, code = 500)]
