d0420f60aaabe796a268b1946de15320d62f814c
Mario Reder <mreder1289@gmail.com>
2021-02-15 16:26:40 +0100

    fix publish path

--- publish.sh
+++ publish.sh
@@ -7,7 +7,7 @@ echo "using git hash $GIT_HASH"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker pull sm64js/sm64js-build || true
-docker build --cache-from=sm64js/sm64js-build -t sm64js/sm64js-build:latest -f ./rust-server/DockerfileBuild .
+docker build --cache-from=sm64js/sm64js-build -t sm64js/sm64js-build:latest -f ./sm64js/DockerfileBuild .
 docker push sm64js/sm64js-build:latest
 docker tag sm64js/sm64js-build sm64js/sm64js-build:$GIT_HASH
 docker push sm64js/sm64js-build:$GIT_HASH
