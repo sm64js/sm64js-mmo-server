aad927d5cecd960ff7e2f07e2cd8aa8c20781960
Mario Reder <mreder1289@gmail.com>
2021-03-17 15:55:45 +0100

    logout without unwrap

--- crates/sm64js-api/src/logout.rs
+++ crates/sm64js-api/src/logout.rs
@@ -1,7 +1,7 @@
 use actix_session::Session;
 use paperclip::actix::{api_v2_operation, web, NoContent};
 use sm64js_auth::Identity;
-use sm64js_db::DbPool;
+use sm64js_db::{DbError, DbPool};
 
 /// Deletes cookie session.
 #[api_v2_operation(tags(Auth))]
@@ -9,13 +9,13 @@ pub async fn post_logout(
     pool: web::Data<DbPool>,
     identity: Identity,
     session: Session,
-) -> NoContent {
+) -> Result<NoContent, DbError> {
     let auth_info = identity.get_auth_info();
 
     let conn = pool.get().unwrap();
-    sm64js_db::delete_session(&conn, auth_info.into_inner()).unwrap();
+    sm64js_db::delete_session(&conn, auth_info.into_inner())?;
 
     session.purge();
 
-    NoContent
+    Ok(NoContent)
 }
