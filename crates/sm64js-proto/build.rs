e04603fcb36988b580dbed70028a0991fcc89179
Mario Reder <mreder1289@gmail.com>
2021-04-14 16:29:35 +0200

    use proto from monorepo

--- crates/sm64js-proto/build.rs
+++ crates/sm64js-proto/build.rs
@@ -2,6 +2,6 @@ fn main() {
     let mut config = prost_build::Config::new();
     config.type_attribute(".", "#[derive(serde::Serialize)]");
     config
-        .compile_protos(&["../../proto/mario.proto"], &["../../proto/"])
+        .compile_protos(&["../../../proto/mario.proto"], &["../../../proto/"])
         .unwrap()
 }
