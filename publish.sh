4ad46225b5741454926f99c39a1408f30d0214d3
Mario Reder <mreder1289@gmail.com>
2021-04-15 06:55:16 +0200

    build changes for docker

--- publish.sh
+++ publish.sh
@@ -6,8 +6,10 @@ echo "using git hash $GIT_HASH"
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
+cp -rf ../proto ./proto
 docker pull sm64js/sm64js-build || true
 docker build --cache-from=sm64js/sm64js-build -t sm64js/sm64js-build:latest .
 docker push sm64js/sm64js-build:latest
 docker tag sm64js/sm64js-build sm64js/sm64js-build:$GIT_HASH
 docker push sm64js/sm64js-build:$GIT_HASH
+rm -rf ./proto
