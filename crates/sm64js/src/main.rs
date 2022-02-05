b37b6a633393ff6510d9f344a1cb2b71b8ac4ebf
Agent-11 / Agent X <44549182+Agent-11@users.noreply.github.com>
2022-02-05 17:19:36 -0500

    Update chat filter again

--- crates/sm64js/src/main.rs
+++ crates/sm64js/src/main.rs
@@ -7,11 +7,15 @@ pub async fn main() -> std::io::Result<()> {
         add_word("crap", Type::SAFE);
         add_word("damn", Type::SAFE);
         add_word("dic", Type::SAFE); // DiC entertainment
+        add_word("fat", Type::SAFE);
         add_word("frick", Type::SAFE);
+        add_word("gay", Type::SAFE);
         add_word("hell", Type::SAFE);
-        add_word("naked", Type::SAFE);
-        add_word("gay", Type::SAFE); // lets hope these aren't misused...
+        add_word("hoe", Type::SAFE);
         add_word("homo", Type::SAFE);
+        add_word("naked", Type::SAFE);
+        add_word("naughty", Type::SAFE);
+        add_word("ur mom", Type::SAFE);
     }
     sm64js::main()?.await
 }
