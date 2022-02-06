141c6d5e3c6126ea606e68d4a0b3eabba28df772
Agent-11 <44549182+Agent-11@users.noreply.github.com>
2022-02-06 17:26:50 -0500

    Update chat filter again after even more testing

--- crates/sm64js/src/main.rs
+++ crates/sm64js/src/main.rs
@@ -11,10 +11,12 @@ pub async fn main() -> std::io::Result<()> {
         add_word("frick", Type::SAFE);
         add_word("gay", Type::SAFE);
         add_word("hell", Type::SAFE);
+        add_word("h i liter", Type::SAFE);
         add_word("hoe", Type::SAFE);
         add_word("homo", Type::SAFE);
         add_word("naked", Type::SAFE);
         add_word("naughty", Type::SAFE);
+        add_word("suck", Type::SAFE);
         add_word("ur mom", Type::SAFE);
     }
     sm64js::main()?.await
