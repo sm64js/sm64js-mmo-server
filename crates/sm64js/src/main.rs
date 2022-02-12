15fe284b73e2eaf1d0b10207071bbb2a6ed9c8d1
Agent-11 <44549182+Agent-11@users.noreply.github.com>
2022-02-11 19:28:34 -0500

    Fix mistake in chat filter

--- crates/sm64js/src/main.rs
+++ crates/sm64js/src/main.rs
@@ -16,7 +16,7 @@ pub async fn main() -> std::io::Result<()> {
         add_word("homo", Type::SAFE);
         add_word("naked", Type::SAFE);
         add_word("naughty", Type::SAFE);
-        add_word("plix", Type::SAFE);
+        add_word("splix", Type::SAFE);
         add_word("suck", Type::SAFE);
         add_word("ur mom", Type::SAFE);
 
